bb22e34110b126b7d3535faf6e212c91072fa7a9
Lyubov Rodomanova <lrodomanova81@gmail.com>
2022-06-11 16:29:44 +0300

    Initial commit

---
+++ src/test/java/lombok/User.java
@@ -0,0 +1,17 @@
+package lombok;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User {
+    private Integer id;
+    private String email;
+    private String name;
+    private String job;
+    @JsonProperty("first_name")
+    private String firstName;
+    @JsonProperty("last_name")
+    private String lastName;
+}
---
+++ src/test/java/lubl/ApiTests.java
@@ -0,0 +1,105 @@
+package lubl;
+
+import lombok.LombokUserData;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static lubl.Specs.*;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ApiTests {
+
+
+    String
+            body = "{\"name\": \"morpheus\",\n" +
+            "    \"job\": \"leader\"}",
+            body2 ="{\"email\": \"sydney@fife\"}",
+            name="morpheus",
+            job="leader";
+
+
+    @Test
+    public void getSingleUser() {
+        given()
+                .spec(request)
+                .when()
+                .get("/users/2")
+                .then()
+                .spec(responseSpec)
+                .extract().response();
+    }
+
+    @Test
+    void userNotFound() {
+        given()
+                .spec(request)
+                .when()
+                .get("/users/23")
+                .then()
+                .spec(response404)
+                .log().body()
+                .body(is("{}"));
+    }
+
+    @Test
+    public void getSingleResource() {
+        given()
+                .spec(request)
+                .get("/unknown/2")
+                .then()
+                .spec(responseSpec)
+                .log().body()
+                .extract().response();
+    }
+
+    @Test
+    void createUser() {
+        LombokUserData data = (LombokUserData) given()
+                .spec(request)
+                .body(body)
+                .when()
+                .post("/users")
+                .then()
+                .spec(response201)
+                .extract().as(LombokUserData.class);
+        assertEquals(name, data.getUser().getName());
+        assertEquals(job, data.getUser().getJob());
+    }
+
+    @Test
+    void deleteUser() {
+        given()
+                .spec(request)
+                .when()
+                .delete("/users/2")
+                .then()
+                .spec(response204);
+    }
+
+    @Test
+    void registerUserUnsuccesfull() {
+        given()
+                .spec(request)
+                .body(body2)
+                .when()
+                .post("/register")
+                .then()
+                .spec(response400);
+    }
+
+    @Test
+    public void checkEmailUsingGroovy() {
+        // @formatter:off
+        given()
+                .spec(request)
+                .when()
+                .get("/users")
+                .then()
+                .log().body()
+                .body("data.findAll{it.email =~/.*?@reqres.in/}.email.flatten()",
+                        hasItem("eve.holt@reqres.in"));
+        // @formatter:on
+    }
+}
